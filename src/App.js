6adcbe02afd4626a446c171ea73bfb294537b21f
RAIMUNDO CALIGA CARDOSO <86314443+RCaligaC@users.noreply.github.com>
2024-05-19 11:15:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App({ xIsNext, squares, onPlay }) {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
       <div className="meuGit">
-        <a href="https://github.com/RCaligaC/JogoDaVelha/">Projeto no meu github =></a>
+        <a href="https://github.com/RCaligaC/JogoDaVelha/">Projeto no github=></a>
       </div>
     </div>
     </>
